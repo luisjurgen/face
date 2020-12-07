30f5506472e74fb4fe8018d167c19c7e95bb6282
luisjurgen <luisjurgenus@gmail.com>
2020-12-06 18:21:16 -0600

    jflower

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ let fondo =document.querySelector('body');
     let mouseCursorY;
 
 //variablesInicializadasParaImagenes
-
-let numFrames=145;
+let numFrames=97;
 let currentFrame=0;
 let secuencia=[];
 
@@ -22,7 +21,6 @@ function nombresImagenes(){
     for(let i=0; i<numFrames; i++){
         let imagenNombre=nFrames(i,4);
         secuencia[i]=imagenNombre;
-        
     }
 }
 
@@ -34,18 +32,19 @@ function mapRange(value, low1, high1, low2, high2) {
 }
 
 
+//////////////touch
 
 //asignacionPosMouse
 window.addEventListener('touchmove',(e)=>{
     // mouseCursorX=e.pageX;
     // mouseCursorY=e.pageY;
-    
-    
     var touchLocation=e.targetTouches[0];
     
     mouseCursorX=touchLocation.pageX;
     mouseCursorY=touchLocation.pageY;
-   
+
+
+
   //posCentro
      
     const centroX=window.innerWidth/2;
@@ -63,12 +62,129 @@ window.addEventListener('touchmove',(e)=>{
     
     let valorRecibido= valorArcotangente(centroX,centroY,mouseCursorX,mouseCursorY);
 
-    let valorNormalizado=Math.round(mapRange(valorRecibido,-180,180,0, numFrames-1));
+    let valorNormalizado=Math.round(mapRange(valorRecibido,-180,180, 0, numFrames-1));
+    console.log(valorNormalizado);
     //intervencion En el Dom
-    murakami.setAttribute("src",`assets/mounstruo${secuencia[valorNormalizado]}.png`);
+    murakami.setAttribute("href",`#s${secuencia[valorNormalizado]}`);
     //colorInterpolacion
     let colorNormalizado=Math.round(mapRange(valorRecibido,-180,180,0,360));
-    fondo.style.background = `hsl(${colorNormalizado},60%,50%)`;
+    fondo.style.background = `hsl(${colorNormalizado},150%,50%)`;
     
 },false);
+
+//cursor touch
+
+
+let mouseCursor = document.querySelector('.cursor');
+let ruedas= document.querySelectorAll('.ruedas');
+
+window.addEventListener('touchmove',(e)=>{
+
+
+    var touchLocation2= e.targetTouches[0];
+    mouseCursor.style.top = touchLocation2.pageY + 'px';
+    mouseCursor.style.left=touchLocation2.pageX+'px';
+
+    
+
+
+});
+
+//scala touch
+ruedas.forEach(rueda=>{
+    rueda.addEventListener('touch',()=>{
+        mouseCursor.classList.add('rueda-especial');
+        
+    });
+    rueda.addEventListener('touchleave',()=>{
+        mouseCursor.classList.remove('rueda-especial');
+      
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////mouse
+
+//asignacionPosMouse
+window.addEventListener('mousemove',(e)=>{
+    mouseCursorX=e.pageX;
+    mouseCursorY=e.pageY;
+    // var touchLocation=e.targetTouches[0];
+    
+    // mouseCursorX=touchLocation.pageX;
+    // mouseCursorY=touchLocation.pageY;
+
+
+
+  //posCentro
+     
+    const centroX=window.innerWidth/2;
+    const centroY=window.innerHeight/2;
+    //calcular el angulo
+
+    function valorArcotangente(centerx1,centery1,x2,y2){
+      
+        let valorArcotangente=Math.atan2(-y2+centery1,-x2+centerx1);
+        let conversionGrados=valorArcotangente/(Math.PI/180);
+        return conversionGrados;
+        // return conversionGrados;
+
+    }
+    
+    let valorRecibido= valorArcotangente(centroX,centroY,mouseCursorX,mouseCursorY);
+
+    let valorNormalizado=Math.round(mapRange(valorRecibido,-180,180, 0, numFrames-1));
+    console.log(valorNormalizado);
+    //intervencion En el Dom
+    murakami.setAttribute("href",`#s${secuencia[valorNormalizado]}`);
+    //colorInterpolacion
+    let colorNormalizado=Math.round(mapRange(valorRecibido,-180,180,0,360));
+    fondo.style.background = `hsl(${colorNormalizado},150%,50%)`;
+    
+},false);
+
+
+
+
+
+//cursor mouse
+
+window.addEventListener('mousemove',(e)=>{
+    mouseCursor.style.top = e.pageY + 'px';
+    mouseCursor.style.left=e.pageX+'px';
+
+
+});
+
+
+//scala mouse
+
+
+ruedas.forEach(rueda=>{
+    rueda.addEventListener('mouseover',()=>{
+        mouseCursor.classList.add('rueda-especial');
+        
+    });
+    rueda.addEventListener('mouseleave',()=>{
+        mouseCursor.classList.remove('rueda-especial');
+      
+    });
+});
+
+
+
 
